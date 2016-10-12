b0ac63820bafe7d84bcd2e06a92c2b1259fc2ab7
Omar Balfaqih <obalfaqih@gmail.com>
2016-10-12 18:13:45 +0800

    Just changed the output :D

--- src/my/edu/taylors/dad/chat/Test.java
+++ src/my/edu/taylors/dad/chat/Test.java
@@ -3,7 +3,7 @@ package my.edu.taylors.dad.chat;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("мљишћэб");
+		System.out.println("THIS IS NEW !!! HURRAAY");
 	}
 
 }
