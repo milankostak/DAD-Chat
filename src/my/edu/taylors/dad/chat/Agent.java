f4997c3df718733e4fb51ec37d6a0970eb839d06
Omar Balfaqih <obalfaqih@gmail.com>
2016-10-12 19:43:15 +0800

    Added Agent Server Updated

---
+++ src/my/edu/taylors/dad/chat/Agent.java
@@ -0,0 +1,48 @@
+package my.edu.taylors.dad.chat;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Agent{
+	private Socket agentSocket = null;
+	
+	public Agent(Socket agentSocket){
+		for(int i = 0; i < 2; i++){
+			new ConnectionHandler();
+		}
+		this.agentSocket = agentSocket;
+	}
+	
+	private static class ConnectionHandler extends Thread {
+		Socket client;
+		BufferedReader br = null;
+		public ConnectionHandler() {
+			start();
+		}
+		
+		public void run(){
+			while(true){
+				try{
+					client = Server.connectionQueue.take();
+					try{
+						PrintWriter pw = new PrintWriter(client.getOutputStream(), true);
+						ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
+						br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+						System.out.println("Client connected with IP: " + client.getRemoteSocketAddress());
+						Auth user = (Auth)ois.readObject();
+						//Handle the messages here
+						pw.flush();
+						client.close();
+					}catch(Exception e){
+						System.out.println("Connection error ...");
+					}
+				}catch(InterruptedException e){
+					System.out.println("Error interrupted");
+				}
+			}
+		}
+	}
+}
--- src/my/edu/taylors/dad/chat/Server.java
+++ src/my/edu/taylors/dad/chat/Server.java
@@ -1,5 +1,57 @@
 package my.edu.taylors.dad.chat;
 
-public class Server {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
 
+public class Server {
+	static Auth[] users = {new Auth("omar", "123", 0),
+			new Auth("test", "123", 0),
+			new Auth("admin", "root", 1),
+			new Auth("agent", "root", 1)};
+	
+	static Socket client = null;
+	static ArrayBlockingQueue<Socket> connectionQueue;
+	
+	public static void main(String args[]){
+		ServerSocket server = null;
+		try{
+			server = new ServerSocket(9999);
+			
+			while(true){
+				client = server.accept();
+				PrintWriter pw = new PrintWriter(client.getOutputStream(), true);
+				ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
+				System.out.println("Client connected with IP: " + client.getRemoteSocketAddress());
+				Auth user = (Auth)ois.readObject();
+				Auth usr = null;
+				// Check if the user matches any of our current users (Authentication)
+				if((usr = user.authenticate(users)) != null){
+					// 1 - Agent, 0 - Guest
+					if(usr.getType() == 0){
+						pw.println("Welcome " + usr.getUsername() + ", you are our guest now !");
+						connectionQueue.put(client);
+					}else{
+						pw.println("Welcome " + usr.getUsername() + ", you are an agent !");
+						new Agent(client);
+					}
+				}else{
+					pw.println("Wrong combination");
+				}
+			}
+			
+		}catch(Exception e){
+			System.out.println("Server Error");
+		} finally{
+			try {
+				server.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
